4ea247aea1749eb80e4c78ec85076a09bd09be1f
Jaimito27 <softjaimito27@gmail.com>
2023-10-30 16:56:58 -0500

    Se agregan iconodos verdadero o falso de acuerdo a la variable de agresivo

---
+++ src/app/pets/interfaces/pet.interface.ts
@@ -0,0 +1,8 @@
+export interface Pet {
+  photo: string;
+  name: string;
+  age: number;
+  type: string;
+  breed: string;
+  dangerous: boolean;
+}
--- src/app/pets/pets.module.ts
+++ src/app/pets/pets.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PetsComponent } from './pages/pets.component';
+import { PetsListComponent } from './components/pets-list/pets-list.component';
+import { RegisterPetComponent } from './components/register-pet/register-pet.component';
+
 
 @NgModule({
-  declarations: [PetsComponent],
+  declarations: [PetsComponent, PetsListComponent, RegisterPetComponent],
   imports: [CommonModule],
 })
 export class PetsModule {}
