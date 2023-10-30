77a0c2ff7cef6d7d4b022f42409ab8bcacbc0a72
Jaimito27 <softjaimito27@gmail.com>
2023-10-30 15:08:58 -0500

    Se crea componente para formulario de asignación de citas, adicional se da un nuevo estilo al componete card de redirección a crear mascota

--- src/app/shedule/shedule.module.ts
+++ src/app/shedule/shedule.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SheduleComponent } from './pages/shedule/shedule.component'
 import { RouterModule } from '@angular/router';
+import { RedirectPetsComponent } from './components/redirect-pets/redirect-pets/redirect-pets.component';
+import { ApplicationFormComponent } from './components/application-form/application-form.component';
 
 @NgModule({
-  declarations: [SheduleComponent],
+  declarations: [SheduleComponent, RedirectPetsComponent, ApplicationFormComponent],
   imports: [CommonModule, RouterModule],
   exports:[
     SheduleComponent
