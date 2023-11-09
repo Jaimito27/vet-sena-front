3daa98e770e6b70a49cc2ad91bbc1089105b44b7
Jaimito27 <softjaimito27@gmail.com>
2023-11-09 16:09:58 -0500

    Se modifican los botones editar y eliminar de la lista de mascotas

--- src/app/pets/pets.module.ts
+++ src/app/pets/pets.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 import { PetsComponent } from './pages/pets.component';
 import { PetsListComponent } from './components/pets-list/pets-list.component';
 import { RegisterPetComponent } from './components/register-pet/register-pet.component';
@@ -7,7 +9,7 @@ import { RegisterPetComponent } from './components/register-pet/register-pet.com
 
 @NgModule({
   declarations: [PetsComponent, PetsListComponent, RegisterPetComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTooltipModule],
   exports:[PetsListComponent, RegisterPetComponent]
 })
 export class PetsModule {}
