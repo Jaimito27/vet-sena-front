8de29256dceaaee5deac7b962cf5e361ca7661d0
Jaimito27 <softjaimito27@gmail.com>
2024-09-19 11:49:15 -0500

    Se establecen botones para despleguar componente de usuarios bloqueados o usuarios activos

--- src/app/users/pages/users.component.ts
+++ src/app/users/pages/users.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+  activeComponent: 'blocked' | 'active' | null = null;
+
+  setActiveComponent(component: 'blocked' | 'active') {
+    this.activeComponent = component;
+  }
 }
