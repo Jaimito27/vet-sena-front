094d7d52eb9f0d1638e7f5141efcf5f56da71e48
Jaimito27 <softjaimito27@gmail.com>
2024-09-22 23:39:49 -0500

    Se crea función para crear usuario desde la ventana registro y funciona correctamente

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RegisterService } from '../../services/register.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterUser } from '../../interfaces/register.interface';
 
 @Component({
   selector: 'app-register',
@@ -12,14 +13,12 @@ export class RegisterComponent {
   passwordMismatch = false;
   emailMismatch = false;
 
-
-
   constructor(
     private formBuilder: FormBuilder,
     private registerService: RegisterService
   ) {}
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.formRegister = this.formBuilder.group({
       doc_type: ['', Validators.required],
       ident_document: ['', [Validators.required, Validators.minLength(4)]],
@@ -28,11 +27,17 @@ export class RegisterComponent {
       phone: ['', [Validators.required, Validators.minLength(10)]],
       username: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
-      email: ['',[Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
-
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          ),
+        ],
+      ],
     });
   }
-
 
   public validatePassword(): void {
     const password2 = document.getElementById('password2') as HTMLInputElement;
@@ -40,14 +45,21 @@ export class RegisterComponent {
     this.passwordMismatch = password !== password2.value;
   }
 
- public validateEmail(): void {
+  public validateEmail(): void {
     const email2 = document.getElementById('email2') as HTMLInputElement;
     const email = this.formRegister.get('email')?.value;
     this.emailMismatch = email !== email2.value;
- }
-
-  public onSubmit(): void{
-    console.log(this.formRegister.value);
   }
 
+  public onSubmit(): void {
+    if (this.formRegister.valid) {
+      const userData: RegisterUser = this.formRegister.value;
+      this.registerService.registerUser(userData).subscribe(
+        (res) => console.log(res),
+        (error) => console.error(error)
+      );
+    } else {
+      console.error('El formulario no es válido');
+    }
+  }
 }
