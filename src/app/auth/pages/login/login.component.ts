4d0071c7cc6aafb75543fdbdcffeb4d461730d72
Jaimito27 <softjaimito27@gmail.com>
2024-09-27 00:16:54 -0500

    Se corrige lógica de forma que una vez el token sea valido, cierre el modal, también que verifique que el token sea valido y asi pueda verificar si el usuario esta bloqueado o las credenciales son correctas. Si existe un error no loguea y arroja una alerta

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -2,26 +2,23 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-
-
-
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-
 })
 export class LoginComponent {
-
   public formLogin!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router
-
-  ) { }
+    private router: Router,
+    private dialogRef: MatDialogRef<LoginComponent>
+  ) {}
 
   ngOnInit(): void {
     this.formLogin = this.formBuilder.group({
@@ -30,23 +27,31 @@ export class LoginComponent {
     });
   }
 
-  onSubmit(): void{
-    if(this.formLogin.valid){
+  onSubmit(): void {
+    if (this.formLogin.valid) {
       this.authService.login(this.formLogin.value).subscribe({
-        next: (response)=>{
-          this.authService.setToken(response.token);
-          this.router.navigate(['/profile']);
-        }, error: (error) => {
-          console.error('Error en el login', error);
+        next: (response) => {
+          if (response.token) { //si el token es valido, entonces se loguea, sino arroja una alerta con el mensaje que sea necesario
+            this.authService.setToken(response.token);
+            this.dialogRef.close();
+            this.router.navigate(['/profile']);
+          } else {
+            this.alertMessage('Error', response.message, 'warning');
+          }
+        },
+        error: (error) => {
           //manejo del error
-        }
-      })
+          this.alertMessage('Error', error.error.message || 'Error de inicio de sesasión', 'error')
+        },
+      });
     }
   }
 
-
-
-
+  public alertMessage(title: string, text: string, icon: SweetAlertIcon): void {
+    Swal.fire({
+      title,
+      text,
+      icon,
+    });
+  }
 }
-
-
